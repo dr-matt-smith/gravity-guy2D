79b4077312fca3d6cd3dc6b645dd6d7f74e0f3a6
Matt Smith <dr_matt_smith@me.com>
2014-03-04 12:36:31 +0000

    v2.0 - 3D part 1 started

---
+++ Scripts/CountdownTimer.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+public class CountdownTimer : MonoBehaviour 
+{
+	private float countdownTimerStartTime;
+	private int countdownTimerDuration;
+
+	public int GetTotalSeconds()
+	{
+		return countdownTimerDuration;
+	}
+
+	public void ResetTimer(int seconds)
+	{
+		countdownTimerStartTime = Time.time;
+		countdownTimerDuration = seconds;
+	}
+	
+	public int GetSecondsRemaining()
+	{
+		int elapsedSeconds = (int)(Time.time - countdownTimerStartTime);
+		int secondsLeft = (countdownTimerDuration - elapsedSeconds);
+		return secondsLeft;
+	}
+
+	public float GetProportionTimeRemaining()
+	{
+		float proportionLeft = (float)GetSecondsRemaining() / (float)GetTotalSeconds();
+		return proportionLeft;
+	}
+}
